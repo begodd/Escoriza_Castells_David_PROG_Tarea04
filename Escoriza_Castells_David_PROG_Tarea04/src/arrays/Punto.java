81c9b52eab804adc379835931cf8bc3eed018bfa
David <David@Davi-PC>
2017-11-22 21:29:01 +0100

    Creamos un constructor con parámetros double x y double y.

--- Escoriza_Castells_David_PROG_Tarea04/src/arrays/Punto.java
+++ Escoriza_Castells_David_PROG_Tarea04/src/arrays/Punto.java
@@ -8,4 +8,11 @@ package arrays;
 public class Punto {
     private double x;
     private double y;
+    
+    public Punto(double x, double y){
+        this.x=x;
+        this.y=y;
+    }
+    
 }
+
