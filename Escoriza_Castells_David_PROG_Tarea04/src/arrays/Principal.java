5bfb34487ceec6c42dc170b82a1b3d89f3f508b8
David <David@Davi-PC>
2017-11-22 22:17:48 +0100

    Creamos un programa que genere la cantidad de triángulos que nosostros introduzcamos por teclado y los almacene en un array.

--- Escoriza_Castells_David_PROG_Tarea04/src/arrays/Principal.java
+++ Escoriza_Castells_David_PROG_Tarea04/src/arrays/Principal.java
@@ -1,11 +1,28 @@
 package arrays;
 
+import utilidades.Entrada;
+
 /**
  *
  * @author David
  */
 public class Principal {
     public static void main(String[] args) {
+        int cantidad;
+        Triangulo array[];
+        Punto A, B, C;
+        
+        System.out.print("Indica cuántos triángulos quieres generar: ");
+        cantidad=Entrada.entero();
+        array=new Triangulo[cantidad];
+                
+        for (int i=0; i<cantidad; i++){
+            A=new Punto((Math.random()*100), (Math.random()*100));
+            B=new Punto((Math.random()*100), (Math.random()*100));
+            C=new Punto((Math.random()*100), (Math.random()*100));
+            array[i]=new Triangulo(A,B,C);
+        }
+        
         
     }
 }
